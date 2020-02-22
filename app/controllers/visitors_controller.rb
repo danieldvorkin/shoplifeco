45600458e77c7aed6e0bf53435a5b836104fc257
Daniel Dvorkin <daniel.dvorkin@ptaginc.com>
2020-02-22 01:54:23 -0500

    Added some routes

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,4 @@
 class VisitorsController < ApplicationController
+    def shop; end
+    def most_recent; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  get '/shop', to: 'visitors#shop'
+  get '/shop/most_recent', to: 'visitors#most_recent'
   devise_for :users
   resources :users
 end
