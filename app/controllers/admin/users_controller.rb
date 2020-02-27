d1604bebfa23cf3b9d8ec692d666b5a091fc260c
Daniel Dvorkin <daniel.dvorkin@ptaginc.com>
2020-02-26 23:22:20 -0500

    locked down some pages

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,11 @@
 module Admin
   class UsersController < Admin::ApplicationController
+    before_action :authenticate_user!
+    before_action :authenticate_admin
+
+    def authenticate_admin
+      redirect_to root_path, alert: 'Not authorized.' unless current_user.role == "admin"
+    end
     # Overwrite any of the RESTful controller actions to implement custom behavior
     # For example, you may want to send an email after a foo is updated.
     #
