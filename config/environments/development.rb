73bab5d434d5d0909d4b1cb04893a62784c87fa7
Daniel Dvorkin <daniel.dvorkin@ptaginc.com>
2020-02-27 00:36:41 -0500

    adjusted the initializers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
     domain: 'yourdomain.com',
-    user_name: Rails.application.credentials.sendgrid_username,
-    password: Rails.application.credentials.sendgrid_password,
+    user_name: Rails.application.secrets.sendgrid_username,
+    password: Rails.application.secrets.sendgrid_password,
     authentication: :login,
     enable_starttls_auto: true
   }
